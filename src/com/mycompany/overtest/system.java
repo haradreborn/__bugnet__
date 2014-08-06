f81645a666e218d2d9de422cc9a81cdd4de52356
haradreborn <haradreborn@gmail.com>
2014-08-06 09:08:47 +0400

    help added

---
+++ src/com/mycompany/overtest/system.java
@@ -0,0 +1,14 @@
+package com.mycompany.overtest;
+
+import android.app.*;
+import android.os.*;
+
+public class system extends Activity
+{
+	/** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+	}
+}
